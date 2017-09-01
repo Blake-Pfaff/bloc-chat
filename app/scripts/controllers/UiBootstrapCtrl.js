bb41137955044714617f073f1ef54648ee273c3d
Thrilldozer <blake.a.pfaff2.com>
2017-09-01 14:29:31 -0500

    stuck

--- app/scripts/controllers/UiBootstrapCtrl.js
+++ app/scripts/controllers/UiBootstrapCtrl.js
@@ -2,15 +2,13 @@
 
     function UiBootstrapCtrl($uibModal) {
         // Im not sure what needs to be injected or if this is the correct set up for this controller.  Im confused about what depencencays need to been injected where and how
-        var modalInstance = $uibModal.open(options);
 
-        modalInstance.result.then(onClose, onCancel);
-
+    var alertTest = function() {
+      alert();
     }
-
 
     angular
         .module('blocChat')
-        .controller('UbiBootstrapCtrl', ['ui.bootstrap']);
+        .controller('UiBootstrapCtrl', ['$uibModal', UiBootstrapCtrl]);
 
 })();
