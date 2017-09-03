26b213be1b534a26f5002412faba434da93d3103
Thrilldozer <blake.a.pfaff2.com>
2017-09-03 11:58:55 -0500

    removes cancel error

--- app/scripts/controllers/UiBootstrapCtrl.js
+++ app/scripts/controllers/UiBootstrapCtrl.js
@@ -1,22 +1,26 @@
 (function() {
 
-    function UiBootstrapCtrl($uibModal) {
-      this.open = function() {
-        $uibModal.open({
-          templateUrl: "templates/modal.html",
-          controller: 'ModalInstanceCtrl',
-          controllerAs: 'modal',
+  function UiBootstrapCtrl($uibModal) {
+    // Controller method to open the modal
+    this.open = function () {
+      // Trigger the modal window and assign it to a modal variable
+      var modal = $uibModal.open({
+        templateUrl: 'templates/modal.html',
+        controller: 'ModalInstanceCtrl',
+        controllerAs: 'modal'
+      });
 
-        })
-      }
+      // Handle the open/close of the modal
+      modal.result.then(function () {
+        console.log('opened!');
+      }, function () {
+        console.log('dismissed!');
+      });
+    };
+  }
 
-}
-
-
-    angular
-        .module('blocChat')
-        .controller('UiBootstrapCtrl', ['$uibModal', UiBootstrapCtrl]);
-
-
+  angular
+    .module('blocChat')
+    .controller('UiBootstrapCtrl', ['$uibModal', UiBootstrapCtrl]);
 
 })();
